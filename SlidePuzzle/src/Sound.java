f7cabba41db5b99e35365d2929d6c2039a141ed0
velislavnikiforov <velinikiforov@gmail.com>
2015-01-30 14:07:34 +0200

    added play and stop

--- SlidePuzzle/src/Sound.java
+++ SlidePuzzle/src/Sound.java
@@ -1,23 +1,67 @@
-import javazoom.jl.player.*;
+import java.io.BufferedInputStream;
 import java.io.FileInputStream;
-
-//added JLayer libary
+import javazoom.jl.player.Player;
 
 public class Sound {
 	
-	public static void playBackgroundMusic(){
-
-		while(true){
-		
-	    try{
-
-	    FileInputStream fis = new FileInputStream("sounds/wipala.mp3");
-	    Player playMP3 = new Player(fis);
-
-	    playMP3.play();
-
-	    }catch(Exception e){System.out.println(e);}
-	    
-		}
+	public static void main(String[] args) {
+		playBackgroundMusic();
 	}
+	
+  private String filename;
+  private Player player;
+
+  //MP3 constructor;
+  
+  public Sound(String filename) {
+    this.filename = filename;
+  }
+
+  // The player;
+  
+  public void play() {
+    try {
+      FileInputStream fis = new FileInputStream(this.filename);
+      BufferedInputStream bis = new BufferedInputStream(fis);
+
+      this.player = new Player(bis);
+    } catch (Exception e) {
+        System.err.printf("%s\n", e.getMessage());
+    }
+
+    new Thread() {
+      @Override
+      public void run() {
+        try {
+          player.play();
+        } catch (Exception e) {
+            System.err.printf("%s\n", e.getMessage());
+        }
+      }
+    }.start();
+  }
+
+  // Close the player;
+  
+  public void close() {
+    if (this.player != null) {
+      this.player.close();
+    }
+  }
+
+
+// Play in an infinite loop;
+
+  public static void playBackgroundMusic() {
+    Sound mp3 = new Sound("sounds/wipala.mp3");
+
+    mp3.play();
+
+    while (true) {
+      if (mp3.player.isComplete()) {
+        mp3.close();
+        mp3.play();
+      }
+    }
+  }
 }

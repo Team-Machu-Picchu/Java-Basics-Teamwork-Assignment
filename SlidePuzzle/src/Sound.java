eb393cd1ea7897bb2f5691a7bb2f604f1641384e
velislavnikiforov <velinikiforov@gmail.com>
2015-01-30 11:36:06 +0200

    Added background music, JLayer libary in folder 'lib', mp3 file in folder 'sounds'

---
+++ SlidePuzzle/src/Sound.java
@@ -0,0 +1,20 @@
+import javazoom.jl.player.*;
+import java.io.FileInputStream;
+
+//added JLayer libary
+
+public class Sound {
+	
+	
+	public static void playBackgroundMusic(){
+
+	    try{
+
+	    FileInputStream fis = new FileInputStream("sounds/wipala.mp3");
+	    Player playMP3 = new Player(fis);
+
+	    playMP3.play();
+
+	    }catch(Exception e){System.out.println(e);}
+	}
+}

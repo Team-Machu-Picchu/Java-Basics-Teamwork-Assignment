351367d4816e478d66951e1016ead9a93edcf126
FilipFilipov <filiprf.softuni@gmail.com>
2015-01-31 17:00:08 +0200

    Add Windows luanch script and remove unused imports from Window.

--- SlidePuzzle/src/Window.java
+++ SlidePuzzle/src/Window.java
@@ -1,25 +1,17 @@
-import java.awt.Color;
-import java.awt.Component;
 import java.awt.Dimension;
-import java.awt.GridLayout;
 import java.awt.SplashScreen;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.AbstractButton;
 import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.JTextArea;
 import javax.swing.SwingUtilities;
-
-import org.omg.CORBA.PUBLIC_MEMBER;
 
 
 
